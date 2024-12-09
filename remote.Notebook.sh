93c42cb51d658276972e14c56911cc8074d35cd7
NolanMace <hwn571869617@gmail.com>
2024-12-09 15:22:29 +0800

    update

---
+++ remote.Notebook.sh
@@ -0,0 +1,25 @@
+# 算法直连
+ssh 18342020306@root@192.168.252.67@192.168.253.250 -p 2222
+# 数据直连
+ssh 18342020306@root@192.168.252.67@192.168.253.250 -p 2222
+# 密码
+Niexc@2023
+# 验证mysql连接
+conda activate kuka
+python
+import pymysql
+
+
+# 验证mysql连接
+import pymysql
+
+pymysql.connect(host="192.168.241.173", user="sjjm", password="KukaHome@2022", database="STORE_SAMPLES")
+
+mysql -h 192.168.241.173 -u sjjm -p
+
+KukaHome@2022
+
+use STORE_SAMPLES
+
+neo4j
+kuka@2024
